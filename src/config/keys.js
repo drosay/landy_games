6d9cc537f5567e45cd30ff58d5234bab5d93cebb
Dilan de la Rosa <drosay@unicartagena.edu.co>
2023-01-30 19:52:44 -0500

    minor bugs fixed

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,12 +1,12 @@
 //Configure when build
 const api_configuration = {
-  //in dev http://localhost:3500/ railway api https://landygames.up.railway.app/
+  //in dev http://localhost:3500/ railway api https://landygames.up.railway.app/ vercel app https://landy-games.vercel.app/
   api_credentials_url: "https://twitch-access-token-private.vercel.app/",
   twitch_games_url: "https://api.twitch.tv/helix/games/top?",
   twitch_videos_url: "https://api.twitch.tv/helix/videos?",
   twitch_query_url: "https://api.twitch.tv/helix/search/categories?query=",
   video_url_format:
-    "https://player.twitch.tv/?video=vVIDEO-ID&parent=localhost&parent=https://drosay.github.io&parent=https://drosay.github.io/landy_games&preload=metadata",
+    "https://player.twitch.tv/?video=vVIDEO-ID&parent=localhost&parent=https://landy-games.vercel.app/&preload=metadata",
 };
 
 module.exports = api_configuration;
