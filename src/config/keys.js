af02dffc2b2a72140c50bae79ea0dcd6e18e2389
Dilan de la Rosa <drosay@unicartagena.edu.co>
2023-01-30 19:56:41 -0500

    minor bugs fixed

--- src/config/keys.js
+++ src/config/keys.js
@@ -6,7 +6,7 @@ const api_configuration = {
   twitch_videos_url: "https://api.twitch.tv/helix/videos?",
   twitch_query_url: "https://api.twitch.tv/helix/search/categories?query=",
   video_url_format:
-    "https://player.twitch.tv/?video=vVIDEO-ID&parent=localhost&parent=https://landy-games.vercel.app/&preload=metadata",
+    "https://player.twitch.tv/?video=vVIDEO-ID&parent=localhost&parent=https://landy-games.vercel.app/&parent=https://vercel.com&preload=metadata",
 };
 
 module.exports = api_configuration;
